5c232e75e5f5d79669217ac19a341a933bcd6596
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-02-28 14:45:09 -0500

    fix: Fixed SARSA/Q-Learning implementation for plotting
    Replicating plots from Sutton 2018 did not work. Adjusting step-size to 0.5 and fixing implementation of SARSA/Q-Learning algorithms resolved bug.

--- CliffWalking.py
+++ CliffWalking.py
@@ -3,7 +3,6 @@ from typing import Dict, List
 import random
 import plotly.graph_objs as go
 
-
 ROOK_ACTIONS = frozenset({(0, -1), (-1, 0), (0, 1), (1, 0)})
 
 
@@ -18,13 +17,13 @@ class GridWorld(object):
         self.rewards = self._generate_rewards(default_reward, other_rewards)
         self.actions = list(map(np.array, actions))
 
-
     def _generate_rewards(self, default_reward, other_rewards):
         # TODO: check if dict() has mutability issues as default method arg
         rewards = self.grid * default_reward
         for coord, r in other_rewards.items():
             rewards[coord] = r
         return rewards
+
 
 class CliffWalking(GridWorld):
 
@@ -39,50 +38,72 @@ class CliffWalking(GridWorld):
         :param action:  performed action
         :return:        (i, j) tuple of the next state and the reward associated with the transition
         """
-
         state = np.array(state)
         next_state = tuple(state + action)
         x, y = next_state
-        reward = self.rewards[tuple(state)]
 
         # check boundary conditions
+        if x < 0 or y < 0 or x >= self.width or y >= self.height:
+            next_state = tuple(state)
+
+        reward = self.rewards[tuple(next_state)]
         if reward == -100:
             next_state = self.start_state
-        elif x < 0 or y < 0 or x >= self.width or y >= self.height:
-            next_state = tuple(state)
 
         return next_state, reward
 
     def epsilon_greedy(self, action_index, epsilon):
+        choices = tuple(set(range(len(self.actions))) - {action_index})
         if random.random() < epsilon:
-            action_index = random.randint(0,len(self.actions)-1)
+            action_index = choices[random.randint(0, len(self.actions) - 2)]
         return action_index
 
-    def control(self, algo: str = 'q_learning', n_episodes: int = 100, alpha: float = 0.1, gamma: float = 1, epsilon: float = 0.1, verbose: bool = False):
+    def control(self, algo: str = 'q_learning', n_episodes: int = 100, alpha: float = 0.5, gamma: float = 1,
+                epsilon: float = 0.1, verbose: bool = False):
         q_shape = [len(self.actions)] + list(self.grid.shape)
-        q_values = np.random.random(size=q_shape)
+        q_values = np.zeros(shape=q_shape)
+
+        if verbose:
+            init_grid = np.zeros((self.width, self.height), dtype=np.int64)
+            grid = init_grid
 
         per_episode_rewards = []
         for e in range(n_episodes):
             reward_sum = 0
-            state = self.start_state
-            while state != self.goal:
-                # take action based on policy
-                a = np.argmax(q_values[:, state[0], state[1]])
-                a = self.epsilon_greedy(a, epsilon)
-                state_next, reward = self.state_transition(state, self.actions[a])
-                q_index = tuple([a] + list(state))
 
-                a_next = [np.argmax(q_values[:, state_next[0], state_next[1]])]
+            # pick initial action
+            state = self.start_state
+            a = np.argmax(q_values[:, state[0], state[1]])
+            a = self.epsilon_greedy(a, epsilon)
+
+            while state != self.goal:
+
+                if verbose:
+                    print(grid.T)
+                    print('current state:', state)
+                    print('next action:', a)
+
+                # take action based on policy
+                if algo == 'q_learning':
+                    a = np.argmax(q_values[:, state[0], state[1]])
+                    a = self.epsilon_greedy(a, epsilon)
+
+                state_next, reward = self.state_transition(state, self.actions[a])
+                a_next = np.argmax(q_values[:, state_next[0], state_next[1]])
                 if algo == 'sarsa':
                     a_next = self.epsilon_greedy(a_next, epsilon)
-                q_index_next = tuple([a_next] + list(state_next))
 
-                q_values[q_index] += alpha * (reward + gamma*(q_values[q_index_next]) - q_values[q_index])
-                state = state_next
+                q_index = a, state[0], state[1]
+                q_index_next = a_next, state_next[0], state_next[1]
+                q_values[q_index] += alpha * (reward + gamma * (q_values[q_index_next]) - q_values[q_index])
+
+                state, a = state_next, a_next
                 reward_sum += reward
-                if reward == -100:
-                    break
+
+                if verbose:
+                    grid = np.zeros((self.width, self.height), dtype=np.int64)
+                    grid[state] = 1
+
             per_episode_rewards.append(reward_sum)
         return per_episode_rewards
 
@@ -94,7 +115,7 @@ class CliffWalking(GridWorld):
                     mode='lines',
                     y=reward,
                     name=algo,
-                )
+            )
             )
 
         layout = dict(
